4751e25391992b676260233610b519ff40c803ff
Hayatunnabi Nabil <imhayatunnabi@gmail.com>
2025-02-26 15:50:59 +0600

    Init modules for Users, Restaurants, Food and Votes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
+import { RestaurantsModule } from './restaurants/restaurants.module';
+import { FoodsModule } from './foods/foods.module';
+import { VotesModule } from './votes/votes.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    UsersModule,
+    RestaurantsModule,
+    FoodsModule,
+    VotesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
